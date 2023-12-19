15dd80ac96ac888136da99b192f4428cbfb2a946
Deevesh Kumar <127090783+deevesh11nov@users.noreply.github.com>
2023-12-19 12:05:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://127.0.0.1:5173",
+    origin: "https://mellow-daifuku-dee5d9.netlify.app",
     methods: ["GET", "POST"],
   },
 });
